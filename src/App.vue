4c5b80ea5ea2f44cd3badf3eb347c30448f66fad
Andreas Karlsson <andreas.karlsson90@hotmail.com>
2020-09-09 16:09:21 +0200

    Added Home component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Home/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+import Home from './components/Home.vue'
+
+export default
+{
+	name: 'App',
+	components:
+	{
+		Home
+	}
 }
 </script>
 
