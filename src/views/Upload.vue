173f0e3e159bd851de804a3e47a73493b8aaed2b
JohannaForsberg93 <johanna_forsberg@msn.com>
2020-09-20 22:33:51 +0200

    Validering och cool effekt på knappen!

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="container">
-    <h1>Upload</h1>
-    <UploadForm />
-  </div>
+    <div class="container">
+        <h1>Upload</h1>
+        <UploadForm />
+    </div>
 </template>
 
 <script>
 import UploadForm from "../components/Upload.vue";
 export default {
-  name: "Upload",
-  components: {
-    UploadForm
-  }
+    name: "Upload",
+    components: {
+        UploadForm,
+    },
 };
 </script>
 
 <style scoped>
 .container {
-  text-align: center;
-  padding: 30px;
+    text-align: center;
+    padding: 30px;
 }
 </style>
